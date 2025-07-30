25b6ef12656e1888b44471fc45acb16c56ffb147
Eyioyo Omatsola <eyo.omat@gmail.com>
2025-07-30 10:19:52 -0700

    Added database functionality using mongoDB to log alert messages and details

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const alertRoutes = require('./routes/alertRoutes');
+const connectDB = require('./config/db');
 require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
 app.use('/alerts', alertRoutes);
+
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
